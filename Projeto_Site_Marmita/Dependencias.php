8346b3fd66eba965081a5732570e61a9926a5263
JonathanPGs <jonathanpgsitemas@outlook.com>
2022-08-07 18:50:08 -0300

    Realizando a estrutura do header, nav e aplicando CSS
    foi add cor, logo, icon

---
+++ Projeto_Site_Marmita/Dependencias.php
@@ -0,0 +1,61 @@
+<style>
+  * {
+    padding: 0;
+    margin: 0 auto;
+  }
+
+  :root {
+    --cor0: #Ffb900;
+    --cor1: linear-gradient(to bottom left, #ffb900 -1%, #ff6600 100%);
+
+  }
+
+
+  body {
+    background-color: #Ffb900;
+
+  }
+
+  header {
+    background: linear-gradient(to bottom left, #ffb900 -1%, #ff6600 100%);
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-family: Arial, verdana, Helvetica, sans-serif;
+    background: linear-gradient(to bottom left, #ff6600 -10%, #ff6600 100%);
+    font-size: 20px;
+    height: 5vh;
+
+  }
+
+  .nav-list {
+    list-style: none;
+    display: flex;
+
+  }
+
+  .nav-list a {
+    letter-spacing: 10px;
+    margin-left: 32px;
+    text-decoration: none;
+    font-weight: bold;
+    color: #Ffb900;
+
+  }
+
+  .img {
+    display: flex;
+  }
+
+  main {
+    margin: 10 50;
+  }
+
+  main h1 {
+    background-color: wheat;
+
+  }
+</style>
---
+++ Projeto_Site_Marmita/index.php
@@ -0,0 +1,45 @@
+<?php
+require_once "Dependencias.php";
+require_once "Dados.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Quentinhas Dona Rita</title>
+  <link rel="shortcut icon" href="imagens/favicon-16x16.png" type="image/x-icon">
+</head>
+
+
+<body>
+
+  <header>
+    <img class="img" src="imagens/LOGO NAV.png" alt="logo html 5">
+
+  </header>
+  <nav>
+
+    <a class="logo"></a>
+    <ul class="nav-list">
+
+      <a href="#">Home</li>
+        <a href="#">Conheça a Dona Rita</li>
+          <a href="#">Contato</li>
+
+    </ul>
+  </nav>
+  </header>
+  <main>
+    <article>
+
+
+    </article>
+  </main>
+  <footer> --------------------------------------------- </footer>
+</body>
+
+</html>
